c3a3db55ec48c80f1eee44e202ee2da7f1bb4bb8
God Human <joaofabriciomoyaalvesoficial@gmail.com>
2025-11-04 21:02:48 -0300

    create a spring boot base

---
+++ restaurante_springboot/src/test/java/com/ghost/restaurante_springboot/RestauranteSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.restaurante_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
